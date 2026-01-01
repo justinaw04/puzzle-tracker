73a12e67ec0591490bc2324b4b97a0d923fc3404
Justina Wang <justinawang@Justinas-New-Mac.local>
2026-01-01 16:52:17 -0500

    error

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Gallery from "@/components/Gallery";
+import Gallery from "../components/Gallery";
 import { supabase } from "@/lib/supabase";
 
 export default function HomePage() {
